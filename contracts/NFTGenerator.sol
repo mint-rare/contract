9b7cff51fd91f47fe4cf62a592f627a9fbd5e416
giseoplee <llgs901@naver.com>
2022-02-21 20:07:29 +0900

    [mint-rare/plan#18] nft generator 추가 및 npm run build 스크립트 변경

--- contracts/NFTGenerator.sol
+++ contracts/NFTGenerator.sol
@@ -1,16 +1,16 @@
 pragma solidity ^0.5.6;
 
-import '../node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721Full.sol';
-import '../node_modules/openzeppelin-solidity/contracts/drafts/Counters.sol';
+import '../node_modules/@klaytn/contracts/token/KIP17/KIP17Full.sol';
+import '../node_modules/@klaytn/contracts/drafts/Counters.sol';
 
-contract NFTGenerator is ERC721Full('NFTGenerator', 'NFT') {
+contract NFTGenerator is KIP17Full('NFTGenerator', 'NFT') {
   using Counters for Counters.Counter;
   Counters.Counter private currentTokenId;
 
   function mintTo(address recipient) public returns (uint256) {
     currentTokenId.increment();
     uint256 newItemId = currentTokenId.current();
-    _safeMint(recipient, newItemId);
+    _mint(recipient, newItemId);
     return newItemId;
   }
 }
